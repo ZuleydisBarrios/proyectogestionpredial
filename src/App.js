725f37858b04849f44728687f61294422ee01224
Zuleydis Barrios <zuly2887@hotmail.com>
2021-11-26 09:14:07 -0500

    Cambios en el archivo style.css

--- src/App.js
+++ src/App.js
@@ -344,14 +344,14 @@ function App() {
 
         <div className="container">
           <div className="copyright">
-            &copy; Copyright <strong><span>Eterna</span></strong>.All Rights Reserved
+            &copy; Copyright <strong><span>Gobernación de Huila</span></strong>.Todos los derechos reservados
           </div>
           <div className="credits">
             {/* <!--All the links in the footer should remain intact. --> */}
             {/* <!--You can delete the links only if you purchased the pro version. --> */}
             {/* <!--Licensing information: https://bootstrapmade.com/license/ --> */}
             {/* <!--Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/ --> */}
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            Diseño: Grupo 11 Equipo8
           </div>
         </div>
       </footer>{/* <!--End Footer --> */}
