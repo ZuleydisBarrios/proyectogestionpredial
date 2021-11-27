286af190065e73866ad8fcdde10fad5c2f32466d
Zuleydis Barrios <zuly2887@hotmail.com>
2021-11-26 22:55:33 -0500

    Ajuste de colores de plantilla archivo css

--- src/App.js
+++ src/App.js
@@ -158,7 +158,7 @@ function App() {
 
             <div className="row">
               <div className="col-lg-6">
-                <img src="assets/img/about.jpg" className="img-fluid" alt="" />
+                <img src="img/about.jpg" className="img-fluid" alt="" />
               </div>
               <div className="col-lg-6 pt-4 pt-lg-0 content">
                 <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
@@ -251,14 +251,14 @@ function App() {
 
             <div className="clients-slider swiper">
               <div className="swiper-wrapper align-items-center">
-                <div className="swiper-slide"><img src="assets/img/clients/client-1.png" className="img-fluid" alt="" /></div>
-                <div className="swiper-slide"><img src="assets/img/clients/client-2.png" className="img-fluid" alt="" /></div>
-                <div className="swiper-slide"><img src="assets/img/clients/client-3.png" className="img-fluid" alt="" /></div>
-                <div className="swiper-slide"><img src="assets/img/clients/client-4.png" className="img-fluid" alt="" /></div>
-                <div className="swiper-slide"><img src="assets/img/clients/client-5.png" className="img-fluid" alt="" /></div>
-                <div className="swiper-slide"><img src="assets/img/clients/client-6.png" className="img-fluid" alt="" /></div>
-                <div className="swiper-slide"><img src="assets/img/clients/client-7.png" className="img-fluid" alt="" /></div>
-                <div className="swiper-slide"><img src="assets/img/clients/client-8.png" className="img-fluid" alt="" /></div>
+                <div className="swiper-slide"><img src=".img/clients/client-1.png" className="img-fluid" alt="" /></div>
+                <div className="swiper-slide"><img src=".img/clients/client-2.png" className="img-fluid" alt="" /></div>
+                <div className="swiper-slide"><img src=".img/clients/client-3.png" className="img-fluid" alt="" /></div>
+                <div className="swiper-slide"><img src=".img/clients/client-4.png" className="img-fluid" alt="" /></div>
+                <div className="swiper-slide"><img src=".img/clients/client-5.png" className="img-fluid" alt="" /></div>
+                <div className="swiper-slide"><img src=".img/clients/client-6.png" className="img-fluid" alt="" /></div>
+                <div className="swiper-slide"><img src=".img/clients/client-7.png" className="img-fluid" alt="" /></div>
+                <div className="swiper-slide"><img src=".img/clients/client-8.png" className="img-fluid" alt="" /></div>
               </div>
               <div className="swiper-pagination"></div>
             </div>
