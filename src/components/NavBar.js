64f632c3d3061f54adca0a31e373eb1b8690158e
Zuleydis Barrios <zuly2887@hotmail.com>
2021-11-27 21:05:03 -0500

    componentes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,24 @@
 const NavBar = ()=>(
     <nav id="navbar" className="navbar">
             <ul>
-              <li><a className="active" href="index.html">Home</a></li>
-              <li><a href="about.html">About</a></li>
-              <li><a href="services.html">Services</a></li>
-              <li><a href="portfolio.html">Portfolio</a></li>
+              <li><a className="active" href="index.html">Inicio</a></li>
+              <li><a href="about.html">Contactenos</a></li>
               <li><a href="team.html">Team</a></li>
-              <li><a href="pricing.html">Pricing</a></li>
               <li><a href="blog.html">Blog</a></li>
-              <li className="dropdown"><a href="none"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
+              <li className="dropdown"><a href="none"><span>Tramites</span> <i className="bi bi-chevron-down"></i></a>
                 <ul>
-                  <li><a href="none">Drop Down 1</a></li>
-                  <li className="dropdown"><a href="none"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
+                  <li><a href="none">Tramites Catastrales</a></li>
+                  <li className="dropdown"><a href="none"><span>Predios</span> <i className="bi bi-chevron-right"></i></a>
                     <ul>
-                      <li><a href="none">Deep Drop Down 1</a></li>
-                      <li><a href="none">Deep Drop Down 2</a></li>
-                      <li><a href="none">Deep Drop Down 3</a></li>
-                      <li><a href="none">Deep Drop Down 4</a></li>
-                      <li><a href="none">Deep Drop Down 5</a></li>
+                      <li><a href="none">Aosciar Predios</a></li>
+                      <li><a href="none">Realizar pagos</a></li>
                     </ul>
                   </li>
                   <li><a href="none">Drop Down 2</a></li>
-                  <li><a href="none">Drop Down 3</a></li>
-                  <li><a href="none">Drop Down 4</a></li>
+              
                 </ul>
               </li>
-              <li><a href="contact.html">Contact</a></li>
+              <li><a href="contact.html">Informes</a></li>
             </ul>
             <i className="bi bi-list mobile-nav-toggle"></i>
           </nav> 
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,8 +3,8 @@ const TopBar = ()=>(
     <section id="topbar" className="d-flex align-items-center">
     <div className="container d-flex justify-content-center justify-content-md-between">
       <div className="contact-info d-flex align-items-center">
-        <i className="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact @example.com</a></i>
-        <i className="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+        <i className="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">gestionpredial@huila.gov.co</a></i>
+        <i className="bi bi-phone d-flex align-items-center ms-4"><span>01 8000 968 716</span></i>
       </div>
       <div className="social-links d-none d-md-flex align-items-center">
         <a href="none" className="twitter"><i className="bi bi-twitter"></i></a>
